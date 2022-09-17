432fcbf62943ad2cf1e82f4579d2387488cd9900
vinicius-hso <viniciushso@icloud.com>
2022-09-17 10:53:21 -0300

    Feat: Profile screen

--- src/screens/Profile/index.js
+++ src/screens/Profile/index.js
@@ -1,12 +1,157 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState, useMemo } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Pressable,
+  Button,
+  Alert,
+} from "react-native";
 
 const Profile = () => {
+  const [name, setName] = useState("Vinnie");
+  const [email, setEmail] = useState("vinnie@gmail.com");
+  const [password, setPassword] = useState("Abc123#");
+
+  //* mock
+  const cpf = useMemo(() => {
+    return "12312312312";
+  }, []);
+
+  async function saveChanges() {
+    const data = {
+      name,
+      email,
+      password,
+    };
+    console.log(data);
+
+    Alert.alert("Salvar alterações", "Alterações salvas com sucesso!", [
+      // {
+      //   text: "Cancel",
+      //   onPress: () => console.log("Cancel Pressed"),
+      //   style: "cancel",
+      // },
+      { text: "OK", onPress: () => console.log("OK Pressed") },
+    ]);
+  }
+
   return (
     <View>
-      <Text>Perfil</Text>
+      <View style={styles.headerContainer}>
+        <Text style={styles.headerTitle}>Perfil</Text>
+        <Text style={styles.headeText}>Visualize ou altere seus dados</Text>
+      </View>
+      <View>
+        <Text style={styles.label}>Nome</Text>
+        <TextInput
+          style={styles.input}
+          placeholder={"Nome"}
+          placeholderTextColor={"#666"}
+          onChangeText={(text) => setName(text)}
+          defaultValue={name}
+        />
+      </View>
+      <View>
+        <Text style={styles.label}>Email</Text>
+        <TextInput
+          style={styles.input}
+          placeholder={"Email"}
+          placeholderTextColor={"#666"}
+          onChangeText={(text) => setEmail(text)}
+          defaultValue={email}
+        />
+      </View>
+      <View>
+        <Text style={styles.label}>CPF</Text>
+        <TextInput
+          style={styles.input}
+          placeholder={"CPF"}
+          placeholderTextColor={"#666"}
+          defaultValue={cpf}
+          editable={false}
+        />
+      </View>
+      <View>
+        <Text style={styles.label}>Senha</Text>
+        <TextInput
+          style={styles.input}
+          placeholder={"Senha"}
+          placeholderTextColor={"#666"}
+          onChangeText={(text) => setPassword(text)}
+          defaultValue={password}
+        />
+      </View>
+      <View style={styles.footer}>
+        <Button
+          onPress={console.log("Excluir conta")}
+          title="Exluir conta"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
+        <Pressable style={styles.button} onPress={saveChanges}>
+          <Text style={styles.buttonText}>Salvar alterações</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: "25%",
+    // margin: 10,
+  },
+  headerTitle: {
+    fontSize: 38,
+    fontFamily: "Poppins_600SemiBold",
+    marginTop: 10,
+  },
+  headeText: {
+    fontSize: 18,
+    fontFamily: "Poppins_400Regular",
+  },
+
+  label: {
+    marginLeft: 20,
+    marginRight: 20,
+    marginBottom: 5,
+    marginTop: 5,
+    color: "#004997",
+  },
+  input: {
+    borderRadius: 10,
+    marginLeft: 20,
+    marginRight: 20,
+    marginBottom: 10,
+    color: "#000",
+    borderColor: "#004997",
+    backgroundColor: "#F6F6F6",
+    borderWidth: 1,
+    height: 45,
+    paddingHorizontal: 10,
+    fontSize: 18,
+  },
+  button: {
+    backgroundColor: "#004997",
+    margin: 20,
+    borderRadius: 10,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "auto",
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "#fff",
+  },
+  footer: {
+    // alignItems: "center",
+    justifyContent: "center",
+  },
+});
 
 export default Profile;
