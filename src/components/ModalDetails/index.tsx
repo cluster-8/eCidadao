4d04e69212e8d32a85611c622cd4d25cb4c1c0d6
vinicius-hso <viniciushso@icloud.com>
2022-09-18 19:56:29 -0300

    Fix: request info displayed

--- src/components/ModalDetails/index.tsx
+++ src/components/ModalDetails/index.tsx
@@ -13,9 +13,12 @@ import {
   InfoRow,
   IconContainer,
   Info,
-} from './styles';
+} from "./styles";
 
 import { Entypo } from "@expo/vector-icons";
+
+import formatReqStatus from "../../utils/formatReqStatus";
+import formatReqType from "../../utils/formatReqType";
 
 interface IModalDetails {
   data: any;
@@ -24,141 +27,81 @@ interface IModalDetails {
 }
 
 const ModalDetails: React.FC<IModalDetails> = (props) => {
-  //   const [modalVisible, setModalVisible] = useState(false);
-
   return (
     <Container>
       <Modal
         animationType="slide"
         transparent={true}
         visible={props.modalVisible}
-        onRequestClose={() => {
-          //   Alert.alert("Modal has been closed.");
-          props.handleClose();
-        }}
+        onRequestClose={() => props.handleClose()}
       >
         <Content onPress={props.handleClose}>
           <ModalContent>
             <ImageContent>
               <Image
-                source={props.data.imagem}
+                source={{ uri: props?.data.image }}
                 imageStyle={{
                   borderTopLeftRadius: 10,
                   borderTopRightRadius: 10,
                 }}
                 resizeMode="cover"
               >
-                <CloseButton
-                  onPress={props.handleClose}
-                >
+                <CloseButton onPress={props.handleClose}>
                   <Entypo name="cross" size={20} />
                 </CloseButton>
               </Image>
             </ImageContent>
 
             <DescriptionContainer>
-              <Title>Solicitação: #0001</Title>
+              <Title>{`Solicitação: #${props?.data.identifier}`}</Title>
 
               <InfoRow>
                 <IconContainer>
-                  <Entypo
-                    name="location-pin"
-                    size={22}
-                    color={"#d1345b"}
-                  />
+                  <Entypo name="location-pin" size={22} color={"#d1345b"} />
                 </IconContainer>
 
-                <Info>
-                  {`Rua Germano Vieira Gonçalves, nº 77 - Bairro Bela Vista - CEP: 12260-000`}
-                </Info>
+                <Info>{props?.data.adress}</Info>
               </InfoRow>
 
               <InfoRow>
                 <IconContainer>
-                  <Entypo
-                    name="calendar"
-                    size={22}
-                    color={"#d1345b"}
-                  />
+                  <Entypo name="calendar" size={22} color={"#d1345b"} />
                 </IconContainer>
 
-                <Info>{`Submetido em: 09/09/2022`}</Info>
+                <Info>{`Submetido em ${new Date(
+                  props?.data.createdAt
+                ).toLocaleDateString()}`}</Info>
               </InfoRow>
 
               <InfoRow>
                 <IconContainer>
-                  <Entypo
-                    name="tag"
-                    size={22}
-                    color={"#d1345b"}
-                  />
+                  <Entypo name="tag" size={22} color={"#d1345b"} />
                 </IconContainer>
 
-                <Info>{`Árvore - Poda`}</Info>
+                <Info>{formatReqType(props?.data.type)}</Info>
               </InfoRow>
 
               <InfoRow>
                 <IconContainer>
-                  <Entypo
-                    name="text"
-                    size={22}
-                    color={"#d1345b"}
-                  />
+                  <Entypo name="text" size={22} color={"#d1345b"} />
                 </IconContainer>
 
-                <Info>{`Necessário a realização da poda do Ypê`}</Info>
+                <Info>{props?.data.description}</Info>
               </InfoRow>
 
               <InfoRow>
                 <IconContainer>
-                  <Entypo
-                    name="warning"
-                    size={22}
-                    color={"#d1345b"}
-                  />
+                  <Entypo name="warning" size={22} color={"#d1345b"} />
                 </IconContainer>
 
-                <Info>{`Aberta`}</Info>
+                <Info>{formatReqStatus(props?.data.status)}</Info>
               </InfoRow>
             </DescriptionContainer>
           </ModalContent>
         </Content>
       </Modal>
-      {/* <Pressable
-        style={[styles.button, styles.buttonOpen]}
-        onPress={() => setModalVisible(true)}
-      >
-        <Text style={styles.textStyle}>Show Modal</Text>
-      </Pressable> */}
     </Container>
   );
 };
-
-// const styles = StyleSheet.create({
-//   textStyle: {
-//     color: "white",
-//     fontWeight: "bold",
-//     textAlign: "center",
-//   },
-//   modalText: {
-//     marginBottom: 15,
-//     textAlign: "center",
-//   },
-//   container: {
-//     flex: 1,
-//   },
-//   iconContainer: {
-//     width: "15%",
-//     flex: 1,
-//   },
-//   info: {
-//     alignItems: "center",
-//     justifyContent: "center",
-//     width: "85%",
-//     fontSize: "14",
-//     fontFamily: "Poppins_400Regular",
-//     // color: "#777777",
-//   },
-// });
 
 export default ModalDetails;
