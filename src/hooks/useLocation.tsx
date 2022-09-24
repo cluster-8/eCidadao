a4a003b94f65744a049405faa1a6cd2feefcb0fc
Hariel <hariel@hotmail.com>
2022-09-24 11:34:47 -0300

    Fix: lint

--- src/hooks/useLocation.tsx
+++ src/hooks/useLocation.tsx
@@ -1,66 +1,77 @@
-import * as Location from 'expo-location';
-import React, { createContext, ReactNode, useContext, useEffect, useMemo, useState } from 'react';
+import * as Location from 'expo-location'
+import React, {
+  createContext,
+  ReactNode,
+  useContext,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react'
 
 // import getLocation from "../utils/getLocation";
 
 interface LocationContextData {
-  coords: any;
+  coords: any
 }
 
 type LocationContextProps = {
-  children: ReactNode;
-};
+  children: ReactNode
+}
 
-const LocationContext = createContext({} as LocationContextData);
+const LocationContext = createContext({} as LocationContextData)
 
 const LocationProvider: React.FC<LocationContextProps> = ({ children }) => {
   const [coords, setCoords] = useState({
     latitude: -23.211820056667055,
     longitude: -45.891863188855105,
-  });
+  })
 
   useEffect(() => {
-    (async function getLocation() {
-      const { status } = await Location.requestForegroundPermissionsAsync();
+    ;(async function getLocation() {
+      const { status } = await Location.requestForegroundPermissionsAsync()
 
       if (status !== 'granted') {
         // setErrorMsg("Permission to access location was denied");
-        console.log('Permission to access location was denied');
-        return;
+        console.log('Permission to access location was denied')
+        return
       }
 
-      const location = await Location.getCurrentPositionAsync({});
+      const location = await Location.getCurrentPositionAsync({})
 
       if (location)
         setCoords({
           latitude: location.coords.latitude,
           longitude: location.coords.longitude,
-        });
-      return location;
-    })();
-  }, []);
+        })
+      return location
+    })()
+  }, [])
 
   useEffect(() => {
-    console.log(coords);
-  }, [coords]);
+    console.log(coords)
+  }, [coords])
 
   const providerValue = useMemo(
     () => ({
       coords,
     }),
-    [coords]
-  );
-  return <LocationContext.Provider value={providerValue}>{children}</LocationContext.Provider>;
-};
+    [coords],
+  )
+  return (
+    <LocationContext.Provider value={providerValue}>
+      {children}
+    </LocationContext.Provider>
+  )
+}
 
 const useLocation = () => {
-  const context = useContext(LocationContext);
+  const context = useContext(LocationContext)
 
   if (!context) {
-    throw new Error('useLocation must be used within an LocationProvider');
+    throw new Error('useLocation must be used within an LocationProvider')
   }
 
-  return context;
-};
+  return context
+}
 
-export { useLocation, LocationProvider };
+export { useLocation, LocationProvider }
